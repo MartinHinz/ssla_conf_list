af50b35ea9d6fd36b0343c9a376b20c91699b1cd
Martin Hinz <martin.hinz@iaw.unibe.ch>
2021-01-25 16:19:22 +0100

    first commit

---
+++ csv2md.R
@@ -0,0 +1,52 @@
+# turning the csv into md files, adapted from zackbatist/open-archaeo
+
+library(glue)
+library(stringr)
+library(dplyr)
+library(purrr)
+library(readr)
+library(jsonlite)
+library(fs)
+source("R/site.R")
+
+conf_list <- readr::read_csv("ssla_conf_list.csv")
+
+# Generate URL-friendly, unique slugs (lowercase, without spaces etc.)
+conf_list %<>% 
+  mutate(slug = clean_slug(Subject)) %>% 
+  group_by(slug) %>% 
+  mutate(slug = unique_slug(cur_data_all()))
+
+# Merge multiple columns
+conf_list %<>% 
+  rowwise() %>% 
+  mutate(
+    links = list(as.list(cnotna(
+      Website = `Website URL`,
+      ProposalURL = `Proposal URL`,
+      SponsorshipURL = `Sponsorship URL`
+      ))),
+    StartDate = `Start Date`,
+    EndDate = `End Date`,
+    tags = list(cnotna(str_split(Tags,"\\|")))
+  ) %>% 
+  ungroup() %>%
+  select(slug,
+         Subject,
+         Description,
+         links,
+         tags,
+         StartDate,
+         EndDate,
+         Location,
+         Country,
+         Venue,
+         `Session Deadline`,
+         `Talk Deadline`
+)
+
+# Generate .md files
+conf_list %>% 
+  rename(title = Subject) %>% 
+  pmap(generate_post_md, path = "content/post/") %>% 
+  invisible()
